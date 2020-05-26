7d51643f5b0ce2c2705654dc1ed302d6f9c7c3e9
summer <56674834+Mrszhao112@users.noreply.github.com>
2020-05-26 13:49:01 +0800

    Create mergeTwoLists21.cpp

---
+++ Array&Linked List/problem/mergeTwoLists21.cpp
@@ -0,0 +1,37 @@
+//将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+//两种解法 递归与迭代
+class Solution {
+public:
+//递归法
+    ListNode* mergeTwoLists1(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) {
+            return l2;
+        } else if (l2 == nullptr) {
+            return l1;
+        } else if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+//迭代法
+    ListNode* mergeTwoLists2(ListNode* l1, ListNode* l2){
+    ListNode* preHead = new ListNode(-1);
+        ListNode* prev = preHead;
+        while (l1 != nullptr && l2 != nullptr) {
+            if (l1->val < l2->val) {
+                prev->next = l1;
+                l1 = l1->next;
+            } else {
+                prev->next = l2;
+                l2 = l2->next;
+            }
+            prev = prev->next;
+        }
+        prev->next = l1 == nullptr ? l2 : l1;
+        return preHead->next;
+    }
+  };
